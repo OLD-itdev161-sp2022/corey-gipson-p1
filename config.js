dc6c431b237a5cb454275f8fac54bfae7c3a543b
Corey <gipsonc@gmatc.matc.edu>
2022-02-22 11:13:42 -0600

    Finished Project 1

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "http://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "28e0b62c32a841bfa78ed7eb95e698fe"
+    }
+}
+
+module.exports = constants;
